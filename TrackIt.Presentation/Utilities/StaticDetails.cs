33e75d910756f5de4a4d61b0840fd2d59f85d7b8
Suberu 'Seyi <suberuseyi@gmail.com>
2024-12-09 00:03:38 +0100

    active link feature implemeted

---
+++ TrackIt.Presentation/Utilities/StaticDetails.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace TrackIt.Presentation.Utilities;
+
+public static class StaticDetails
+{
+    public static string IsActive(this IHtmlHelper html, string controller, string action, string cssClass = "active-link")
+    {
+        var routeData = html.ViewContext.RouteData;
+        var routeAction = routeData.Values["action"]?.ToString();
+        var routeController = routeData.Values["controller"]?.ToString();
+
+        return controller == routeController && action == routeAction ?
+            cssClass :
+            string.Empty;
+    }
+}
